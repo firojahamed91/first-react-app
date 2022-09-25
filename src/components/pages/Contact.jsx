6cda462f8106d5e53f260f7911ee2279f9aa0272
firoj <firojahamed91@gmail.com>
2022-09-25 14:44:47 +0600

    added all files in git

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -33,51 +33,51 @@ const formSubmit=(e)=>{
                <div className="row">
                    <div className="col-md-6 col-10 mx-auto">
                       <form onSubmit={formSubmit} className="border p-4 shadow">
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">FullName</label>
+                        <div className="mb-3">
+                            <label for="exampleFormControlInput1" className="form-label">FullName</label>
                             <input 
                             type="name" 
-                            class="form-control" 
+                            className="form-control" 
                             id="exampleFormControlInput1" 
                             name="fullname"
                             value={data.fullname}
                             onChange={InputEvent}
                             placeholder="Enter your name"/>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label"> Phone </label>
+                        <div className="mb-3">
+                            <label for="exampleFormControlInput1" className="form-label"> Phone </label>
                             <input 
                             type="number" 
-                            class="form-control" 
+                            className="form-control" 
                             id="exampleFormControlInput1" 
                             name="phone"
                             value={data.phone}
                             onChange={InputEvent}
                             placeholder="Mobile Number"/>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <div className="mb-3">
+                            <label for="exampleFormControlInput1" className="form-label">Email address</label>
                             <input 
                             type="email" 
-                            class="form-control" 
+                            className="form-control" 
                             id="exampleFormControlInput1" 
                             name="email"
                             value={data.email}
                             onChange={InputEvent}
                             placeholder="name@example.com"/>
                         </div>
-                        <div class="mb-3">
-                                <label for="exampleFormControlTextarea1" class="form-label">Messege</label>
+                        <div className="mb-3">
+                                <label for="exampleFormControlTextarea1" className="form-label">Messege</label>
                                 <textarea 
-                                class="form-control" 
+                                className="form-control" 
                                 id="exampleFormControlTextarea1" 
                                 name="msg"
                                 value={data.msg}
                                 onChange={InputEvent}
                                 rows="3"></textarea>
                         </div>
-                         <div class="col-12">
-                            <button class="btn btn-outline-primary" type="submit">Submit form</button>
+                         <div className="col-12">
+                            <button className="btn btn-outline-primary" type="submit">Submit form</button>
                         </div>
                       </form>
                    </div>
